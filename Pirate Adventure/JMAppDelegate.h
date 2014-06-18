4f13bb25f6f0a382f8876a7f90bacbe2c36134df
JONATHAN MARIANO <jonathanmariano@jonathans-mbp.home>
2014-06-18 13:52:57 -0700

    Initial Commit

---
+++ Pirate Adventure/JMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMAppDelegate.h
+//  Pirate Adventure
+//
+//  Created by JONATHAN MARIANO on 6/18/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Adventure/JMViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMViewController.h
+//  Pirate Adventure
+//
+//  Created by JONATHAN MARIANO on 6/18/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMViewController : UIViewController
+
+@end
